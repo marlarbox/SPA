4e9b9ff2a24ac796d05b4b79ae65ba20b64db753
Unknown <marlarbox@gmail.com>
2018-07-01 20:39:32 -0400

    Review 1

---
+++ Mod1-Solution/app.js
@@ -0,0 +1,41 @@
+(function(){
+    'use strict';
+
+    angular.module("LunchCheck",[])
+    .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope){
+        
+        $scope.CheckSize = function(){
+            if($scope.lunchList){
+                $scope.message = CheckLunchSize($scope.lunchList);
+            }
+            else{
+                $scope.message = "Please enter data first";
+            }
+        };
+
+       $scope.lunchList = "";
+       $scope.message = "dd";
+    }
+  
+    function CheckLunchSize(lunchlist){
+        var lunchSize = lunchlist.split(",").length;
+
+        console.log(lunchlist);
+        console.log(lunchSize);
+
+        if( lunchSize <= 3){
+            return "Enjoy!";
+        }
+        else if (lunchSize > 3)
+        {
+            return "Too much!";
+        }
+
+        return "Error : Shouldn't reach here...";
+    }
+
+})();
